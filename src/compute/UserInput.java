6d59e25916cdce4cf2a872f4d6814ff183c883a3
eawells <elizabeth.a.wells1@gmail.com>
2017-06-27 15:56:30 -0500

    Started working on changes to account for square roots in the equation

--- src/compute/UserInput.java
+++ src/compute/UserInput.java
@@ -23,11 +23,14 @@ public class UserInput {
 		s.close();
 	}
 	
+	//this class should read the user input char by char to figure out what
+	//needs to be pushed on the stacks
+	
 	//use a stack to calculate the value of the expression
 	public double read(String expression){
 		//1. go through each char in the expression,
 		//2. if the char is an op, then push that op into the s_op stack
-		//3. push all the chars behind that op (until the last op) into the s_num stack, casted as doubles
+		//	if the char is not an op, then concatenate it to the number string, add to s_num casted as double
 		//if the op is + or - or (, then continue pushing
 		//if the op is ), then find the value of the expression until reaching ( in s_op
 		//if the op is * or /, then pop the last number and perform the op on it and the next number
@@ -35,83 +38,87 @@ public class UserInput {
 		
 		//to keep track of where the last op was
 		int j = -1;
+		String num = "";
 		Stack<Character> s_op = new Stack<Character>();
 		Stack<Double> s_num = new Stack<Double>();
 		for(int i = 0; i<expression.length(); i++){
-			if (expression.charAt(i) == '*' || expression.charAt(i) == '/' || expression.charAt(i) == '+' || expression.charAt(i) == '-'){
-				if(i != 0){
-					s_op.push(expression.charAt(i));
-					j=i;
-				}
-				else{
-					//get the double that was before the op
-					double doub = getDouble(j,i,expression);
-					//push the number		
-					s_num.push(doub);
-					if(!s_op.isEmpty()){
-						if(s_op.peek() == ')'){
-							//
-						}
-						if(s_op.peek() == '*'){
-							ans = s_num.pop()*s_num.pop();
-							s_num.push(ans);
-							s_op.pop();
-						}
-						else if(s_op.peek() == '/'){
-							double one = s_num.pop();
-							double two = s_num.pop();
-							ans = two/one;
-							s_num.push(ans);
-							s_op.pop();
-						}
+			if (expression.charAt(i) == '*' || expression.charAt(i) == '/' || expression.charAt(i) == '+' 
+					|| expression.charAt(i) == '-' || expression.charAt(i)== ')' || expression.charAt(i)=='(' || expression.charAt(i)=='s'){
+				//do something if the op is square root
+				//get the whole sqrt and push s on the stack
+				
+				//else
+				s_op.push(expression.charAt(i));
+					
+					if(i != 0){
+						s_num.push(Double.parseDouble(num));
+						num = "";
 					}
-					s_op.push(expression.charAt(i));
-					j=i;
-					//System.out.println(doub);
-					//System.out.println(expression.charAt(i));
 				}
+			else{
+				num += expression.charAt(i);
 			}
 		}
-		//push the last double
-		double doub = getDouble(j, expression.length() ,expression);
-		s_num.push(doub);
-		while(!s_op.isEmpty() && (s_num.size()>1)){
-			if(s_op.peek()== '*'){
-					ans = s_num.pop()*s_num.pop();
-			}
-			else if(s_op.peek()== '/'){
-					double one = s_num.pop();
-					double two = s_num.pop();
-					ans = two/one;
-			}
-			else if(s_op.peek()== '+'){
-					ans = s_num.pop()+s_num.pop();
-			}
-			else if(s_op.peek()== '-'){
-					double one1 = s_num.pop();
-					double two2 = s_num.pop();
-					ans = two2-one1;
-			}
-			s_num.push(ans);
-			s_op.pop();
-		}
+		s_num.push(Double.parseDouble(num));
+//			if (expression.charAt(i) == '*' || expression.charAt(i) == '/' || expression.charAt(i) == '+' || expression.charAt(i) == '-'){
+//				if(i != 0){
+//					s_op.push(expression.charAt(i));
+//					j=i;
+//				}
+//				else{
+//					//get the double that was before the op
+//					double doub = getDouble(j,i,expression);
+//					//push the number		
+//					s_num.push(doub);
+//					if(!s_op.isEmpty()){
+//						if(s_op.peek() == ')'){
+//							//
+//						}
+//						if(s_op.peek() == '*'){
+//							ans = s_num.pop()*s_num.pop();
+//							s_num.push(ans);
+//							s_op.pop();
+//						}
+//						else if(s_op.peek() == '/'){
+//							double one = s_num.pop();
+//							double two = s_num.pop();
+//							ans = two/one;
+//							s_num.push(ans);
+//							s_op.pop();
+//						}
+//					}
+//					s_op.push(expression.charAt(i));
+//					j=i;
+//					//System.out.println(doub);
+//					//System.out.println(expression.charAt(i));
+//				}
+//			}
+//		}
+//		//push the last double
+//		double doub = getDouble(j, expression.length() ,expression);
+//		s_num.push(doub);
+//		while(!s_op.isEmpty() && (s_num.size()>1)){
+//			if(s_op.peek()== '*'){
+//					ans = s_num.pop()*s_num.pop();
+//			}
+//			else if(s_op.peek()== '/'){
+//					double one = s_num.pop();
+//					double two = s_num.pop();
+//					ans = two/one;
+//			}
+//			else if(s_op.peek()== '+'){
+//					ans = s_num.pop()+s_num.pop();
+//			}
+//			else if(s_op.peek()== '-'){
+//					double one1 = s_num.pop();
+//					double two2 = s_num.pop();
+//					ans = two2-one1;
+//			}
+//			s_num.push(ans);
+//			s_op.pop();
+//		}
 		
 		return ans;
-	}
-	
-	private double compute(char op, double left, double right){
-		switch (op){
-		case '+':
-			return left+right;
-		case '-':
-			return left-right;
-		case '/':
-			return left/right;
-		case '*':
-			return left*right;
-		}
-		return left;
-		
 	}
 	
 	private double getDouble(int j, int i, String expression){
