b37d23716af656b3fdf2447901a0f5505753fbcd
eawells <elizabeth.a.wells1@gmail.com>
2017-06-26 21:58:40 -0500

    Started to modify code to include expressions with parenthesis

--- src/compute/UserInput.java
+++ src/compute/UserInput.java
@@ -5,7 +5,6 @@ public class UserInput {
 
 	public static void main(String args[]) {
 		UserInput us = new UserInput();
-		
 	}
 	
 	private String expression1;
@@ -17,60 +16,20 @@ public class UserInput {
 		Scanner s = new Scanner(System.in);
 		expression1 = s.nextLine();
 		while(!expression1.equals("exit")){
-			System.out.println("The answer is " + this.calculate(expression1));
+			System.out.println("The answer is " + this.read(expression1));
 			System.out.println("Please enter an expression: ");
 			expression1 = s.nextLine();
 		}
 		s.close();
-//		Scanner s = new Scanner(System.in);
-//		System.out.println("Enter an operator please: ");
-//		op = s.next().charAt(0);
-//		//if char is not operator, print error
-//		System.out.println("Please enter var1: ");
-//		var1 = s.next();
-//		//if var is not ints and operator, print error
-//		System.out.println("Please enter var2: ");
-//		var2 = s.next();
-//		System.out.println("The answer is " + this.calculate(var1 + op + var2));
-//		s.close();
 	}
 	
-//	//count the number of ops in the string and store them in an arraylist
-//	//make a double array of that size plus one
-//	//store each number in that array
-//	public ArrayList<Character> parseForOps(String var){
-//		//for each char in var, check if it is an op. If it is, then 
-//		//store it in arraylist
-//		ArrayList<Character> ops = new ArrayList<Character>();
-//		for (int i = 0; i < var.length(); i++) {
-//			if(var.charAt(i) == '*' || var.charAt(i) == '/' || var.charAt(i) == '+' || var.charAt(i) == '-'){
-//				ops.add(var.charAt(i));
-//			}
-//		}
-//		//System.out.println(ops.toString());
-//		return ops;		
-//		
-//	}
-//	public String[] parseForNumbers(String var){
-//		String delims = "[+\\-*/]+"; 
-//		String[] doubles = var.split(delims);
-//		//System.out.println(doubles[0] + doubles[1] + doubles[2]);
-//		return doubles;		
-//	}
-//		
-//	//now that we have the doubles and the ops, we need to calculate the answer
-//	public double calculate(String[] op, String[] numbers){
-//		//for each op, push the op and push the next two numbers
-//		
-//		return 0;
-//	}
-	
 	//use a stack to calculate the value of the expression
-	public double calculate(String expression){
+	public double read(String expression){
 		//1. go through each char in the expression,
 		//2. if the char is an op, then push that op into the s_op stack
 		//3. push all the chars behind that op (until the last op) into the s_num stack, casted as doubles
-		//if the op is + or -, then continue pushing
+		//if the op is + or - or (, then continue pushing
+		//if the op is ), then find the value of the expression until reaching ( in s_op
 		//if the op is * or /, then pop the last number and perform the op on it and the next number
 		double ans = 0;
 		
@@ -90,6 +49,9 @@ public class UserInput {
 					//push the number		
 					s_num.push(doub);
 					if(!s_op.isEmpty()){
+						if(s_op.peek() == ')'){
+							//
+						}
 						if(s_op.peek() == '*'){
 							ans = s_num.pop()*s_num.pop();
 							s_num.push(ans);
@@ -135,6 +97,21 @@ public class UserInput {
 		}
 		
 		return ans;
+	}
+	
+	private double compute(char op, double left, double right){
+		switch (op){
+		case '+':
+			return left+right;
+		case '-':
+			return left-right;
+		case '/':
+			return left/right;
+		case '*':
+			return left*right;
+		}
+		return left;
+		
 	}
 	
 	private double getDouble(int j, int i, String expression){
